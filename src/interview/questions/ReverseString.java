f03206696627f369344b097e00de357643348349
Ross Bille <rossjbille@gmail.com>
2014-03-19 17:06:18 +1100

    added some documentation to reverse string

--- src/interview/questions/ReverseString.java
+++ src/interview/questions/ReverseString.java
@@ -6,7 +6,14 @@ package interview.questions;
  */
 public class ReverseString
 {
-	private static String reverse(String str, int index, String reversed){
+	/**
+	 * Reverse a String without using external or standard classes/functions using recursion
+	 * @param str the string to reverse
+	 * @param index the current end of the string (starts as str.length())
+	 * @param reversed the partial reversed string
+	 * @return the reversed string
+	 */
+	public static String reverse(String str, int index, String reversed){
 		//base case
 		if(index == 1){
 			return reversed + str.charAt(--index);
@@ -22,7 +29,7 @@ public class ReverseString
 		);
 	}
 	public static void main(String[] args){
-		String str = "tests";
+		String str = "level";  // :)
 		System.out.println("The original string is: " +str);
 		System.out.println("Reveresed: " + reverse(str,str.length(),""));
 		System.out.println("Reversed back: "+reverse(reverse(str, str.length(), ""), str.length(), ""));
